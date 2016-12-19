877d922c1ad31abd8e3b788751e9e7ccb1f23e0b
Vitor Horta <vitor.horta@gmail.com>
2016-12-19 10:41:48 -0200

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './Grid.js';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to Teste</h2>
+                </div>
+                <Example/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Grid.js
@@ -0,0 +1,137 @@
+import React, {Component} from 'react';
+import ReactDataGrid from 'react-data-grid';
+import './App.css';
+
+//helper to generate a random date
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
+}
+
+//helper to create a fixed number of rows
+function createRows(numberOfRows) {
+    var _rows = [];
+    for (var i = 1; i < numberOfRows; i++) {
+        _rows.push({
+            data_emissao: i,
+            classificacao: 'Task ' + i,
+            fornecedor: Math.min(100, Math.round(Math.random() * 110)),
+            condicao: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+            issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+            startDate: randomDate(new Date(2015, 3, 1), new Date()),
+            completeDate: randomDate(new Date(), new Date(2016, 0, 1))
+        });
+    }
+    return _rows;
+}
+
+var PercentCompleteFormatter = React.createClass({
+    render: function () {
+        if (!this.props.value) return (<div></div>);
+        var percentComplete = "R$ " + this.props.value;
+
+        return (
+            <div>
+                {percentComplete}
+            </div>);
+    }
+});
+
+//Columns definition
+var columns = [
+    {
+        key: 'data_emissao',
+        name: 'Data Emissão',
+        width: 80,
+        height: 10,
+        editable: true
+    },
+    {
+        key: 'classificacao',
+        name: 'Classificação',
+        editable: true
+    },
+    {
+        key: 'fornecedor',
+        name: 'Fornecedor',
+        editable: true
+    },
+    {
+        key: 'condicao',
+        name: 'Condição',
+        editable: true
+    },
+    {
+        key: 'doc',
+        name: 'Doc',
+        editable: true
+    },
+    {
+        key: 'cheque',
+        name: 'Cheque',
+        editable: true
+    },
+    {
+        key: 'parcela',
+        name: 'Parcela',
+        editable: true
+    },
+    {
+        key: 'valor',
+        name: 'Valor',
+        editable: true,
+        formatter: PercentCompleteFormatter
+
+    },
+    {
+        key: 'data_vencimento',
+        name: 'Data Vencimento',
+        editable: true
+    },
+    {
+        key: 'data_baixa',
+        name: 'Data Baixa',
+        editable: true
+    },
+    {
+        key: 'conta',
+        name: 'Conta',
+        editable: true
+    }
+];
+
+
+var Example = React.createClass({
+
+    getInitialState: function () {
+        return {rows: createRows(1000)}
+    },
+
+    rowGetter: function (rowIdx) {
+        return this.state.rows[rowIdx]
+    },
+
+    handleRowUpdated: function (e) {
+        //merge updated row with current row and rerender by setting state
+        var rows = this.state.rows;
+        Object.assign(rows[e.rowIdx], e.updated);
+        this.setState({rows: rows});
+        console.log(e);
+    },
+
+    render: function () {
+        return (
+            <ReactDataGrid
+                enableCellSelect={true}
+                columns={columns}
+                rowGetter={this.rowGetter}
+                rowHeight={50}
+                rowsCount={this.state.rows.length}
+                minHeight={500}
+                onRowUpdated={this.handleRowUpdated}/>
+        )
+    }
+
+});
+
+export default Example;
